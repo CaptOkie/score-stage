49593aaecba8006521bf4841ae6f522e7a0f2057
CaptOkie <henriknoetze@cmail.carleton.ca>
2016-12-18 19:20:30 -0500

    Improved extra spacing with modifiers

--- WebContent/resources/js/components/music-score.js
+++ WebContent/resources/js/components/music-score.js
@@ -4,10 +4,21 @@ angular.module('score-stage')
     var WIDTH = 1000;
     var MAX_WIDTH = WIDTH - 1;
     
+    function getExtraWidth(stave) {
+        return (stave.getNoteStartX() + MAX_WIDTH - stave.getNoteEndX())
+            + stave.getModifiers().reduce(function(prev, curr) {
+                if (curr.getPosition() === Vex.Flow.StaveModifier.Position.END) {
+                    return prev + curr.getWidth() + curr.getPadding();
+                }
+                return prev;
+            }, 0);
+    }
+    
     function link(scope, element, attrs) {
         
-        var bars = [0,0,0,0,0,0].fill(new SS.Bar(new SS.TimeSignature(3, 4), [ new SS.Tick('q', [ new SS.Note('a', 4) ]), new SS.Tick('q', [ new SS.Note('b', 4) ]),
-            new SS.Tick('q', []), new SS.Tick('q', []), new SS.Tick('q', [ new SS.Note('c', 5), new SS.Note('d', 5), new SS.Note('e', 5) ]) ]));
+        var bars = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+            .fill(new SS.Bar(new SS.TimeSignature(3, 4), [ new SS.Tick('q', [ new SS.Note('a', 4) ]), new SS.Tick('q', [ new SS.Note('b', 4) ]),
+                new SS.Tick('q', []), new SS.Tick('q', []), new SS.Tick('q', [ new SS.Note('c', 5), new SS.Note('d', 5), new SS.Note('e', 5) ]) ]));
         
         var renderer = new Vex.Flow.Renderer(element[0], Vex.Flow.Renderer.Backends.SVG);
         var context = renderer.getContext();
@@ -37,11 +48,8 @@ angular.module('score-stage')
             
             var width = formatter.preCalculateMinTotalWidth([ voice ]) * 2;
             formatter.format([ voice ], width);
-            width += stave.getNoteStartX() + MAX_WIDTH - stave.getNoteEndX();
-            var modWidths = stave.getModifiers().reduce(function(prev, curr) {
-                return prev + curr.getWidth() + curr.getPadding();
-            }, 0);
-            width += modWidths;
+            var extraWidth = getExtraWidth(stave);
+            width += extraWidth;
 
             var newRow = (row.width + width) > MAX_WIDTH;
             if (newRow) {
@@ -55,22 +63,18 @@ angular.module('score-stage')
             }
             stave.setX(row.left).setY(row.top);
 
-//            var stave = new Vex.Flow.Stave(row.left, row.top, width);
-//            if (newRow) {
-//                stave.addClef(clef);
-//            }
-//            else if (!prev) {
-//                stave.addClef(clef);
-//                stave.addTimeSignature(bar.time.vexFormat());
-//            }
-//            console.log(row.left, stave.getNoteStartX())
-//            console.log(stave.getNoteStartX() - (row.left - width))
-//            console.log(row.left - stave.getNoteEndX())
-//            var extraWidth = (stave.getNoteStartX() - (row.left - width)) + (row.left - stave.getNoteEndX());
-//            width += extraWidth;
-//            row.width += extraWidth;
-//            row.left += extraWidth;
-//            stave.setWidth(width);
+            if (newRow) {
+                stave.addClef(clef);
+            }
+            else if (!prev) {
+                stave.addClef(clef);
+                stave.addTimeSignature(bar.time.vexFormat());
+            }
+            
+            extraWidth = getExtraWidth(stave);
+            width += extraWidth;
+            row.width += extraWidth;
+            row.left += extraWidth;
             
             stave.setWidth(width).setContext(context).draw();
             voice.draw(context, stave);
