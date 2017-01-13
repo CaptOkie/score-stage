4f4ff8a1c9b0bf220b252d039de942a26fca92b3
CaptOkie <henriknoetze@cmail.carleton.ca>
2017-01-13 01:19:16 -0500

    Deleted angular items

--- score-stage-server/WebContent/resources/js/app.js
+++
@@ -1,4 +0,0 @@
-angular.module('score-stage', [ 'ngAnimate', 'ngAria', 'ngMaterial', 'ngMessages', 'ngSanitize' ])
-.config(function($mdThemingProvider) {
-    $mdThemingProvider.enableBrowserColor();
-});
